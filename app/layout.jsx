0ffdf85d0a9af2afd1e48e860b899817d9a383b4
koome <frankoomef@gmail.com>
2023-08-28 21:07:22 +0300

    basic layout of app

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { FaComputer, FaUserGear, FaChevronRight, FaComputerMouse, FaCartPlus } from "react-icons/fa6";
+import { FaComputer, FaUserGear, FaChevronRight, FaComputerMouse, FaCartPlus, FaCartShopping } from "react-icons/fa6";
 import { RiDashboard3Line, RiUserSettingsLine, RiComputerFill } from "react-icons/ri";
 import { MdMenuOpen, MdAccountCircle, MdOutlineShoppingCartCheckout, MdStorage, MdComputer, MdOutlineDesktopWindows } from "react-icons/md";
 import { HiSwitchVertical } from "react-icons/hi"
@@ -27,27 +27,31 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <div>
-        <h1 className="text-center font-bold text-lg text-white fixed overflow-hidden w-full">
-          <Link href="/">Mburus Tech</Link>
-        </h1>
-        <hr />
+      <body className="">
+      <div> 
+        <div className="m-x-6 flex font-bold text-lg text-white sticky overflow-x-hidden w-full min-h-fit justify-between navbar bg-green-800">
+          <h1 className='justify-center ms-10'>
+            <Link href="/">Mburus Tech</Link>
+          </h1>
+          <p className="flex me-10 ">
+            <MdAccountCircle size={28} color="white" title='Logged in user' /> Franklin 
+          </p>
+        </div>
         <div className="flex flex-row">
-          <div className="navbar bg-gray-900 w-1/6 h-screen overflow-y-auto">
-            <div className="links-container flex flex-col justify-start py-4">
+          <div className="navbar bg-green-800 w-1/6 h-screen overflow-y-auto">
+            <div className="links-container flex flex-col justify-start py-2">
               <NavLink
                 label="Home Page"
                 icon={<AiFillHome size={28} color="white" />}
-                className="text-white text-lg hover:bg-gray-950"
+                className="text-white text-lg hover:bg-green-950"
                 variant="subtle"
                 component={Link}
                 href="/"
               />
               <NavLink
                 label="Account"
-                icon={<MdAccountCircle size={28} color="white" />}
-                className="text-white text-lg hover:bg-gray-950"
+                icon={<RiUserSettingsLine size={28} color="white" />}
+                className="text-white text-lg hover:bg-green-950"
                 variant="subtle"
                 component={Link}
                 href="/user"
@@ -55,58 +59,58 @@ export default function RootLayout({ children }) {
               <NavLink
                 label="Categories"
                 icon={<BiCategory size={28} color="white" />}
-                className="text-white text-lg hover:bg-gray-950"
+                className="text-white text-lg hover:bg-green-950"
                 variant="subtle"
                 rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
-                childrenOffset={40}
+                childrenOffset={20}
               >
                 <NavLink
                   label="Desktops"
                   icon={<PiComputerTowerFill size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/desktops"
                 />
                 <NavLink
                   label="Monitors"
                   icon={<MdOutlineDesktopWindows size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/monitors"
                 />
                 <NavLink
                   label="Laptops"
                   icon={<MdComputer size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/laptops"
                 />
                 <NavLink
                   label="Accessories"
                   icon={<BiCategory size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   variant="subtle"
                   rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
-                  childrenOffset={40}
+                  childrenOffset={20}
                 >
                   <NavLink
                   label="Mouse"
                   icon={<FaComputerMouse size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/accessories/mouse"
                 />
                 <NavLink
                   label="Keyboards"
                   icon={<BsKeyboardFill size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/accessories/keyboards"
                 />
                 <NavLink
                   label="Storage"
                   icon={<MdStorage size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/accessories/storage"
                 />
@@ -114,29 +118,29 @@ export default function RootLayout({ children }) {
                 <NavLink
                   label="Projectors"
                   icon={<FcVideoProjector size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/projectors"
                 />
                 <NavLink
                   label="Networking"
                   icon={<BiSolidServer size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   variant="subtle"
                   rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
-                  childrenOffset={40}
+                  childrenOffset={20}
                 >
                   <NavLink
                   label="Servers"
                   icon={<BiSolidServer size={28} color="white" />}
-                  className="text-white text-lg hover:bg-gray-950"
+                  className="text-white text-lg hover:bg-green-950"
                   component={Link}
                   href="/categories/networking/servers"
                   />
                   <NavLink
                     label="Switches"
                     icon={<HiSwitchVertical size={28} color="white" />}
-                    className="text-white text-lg hover:bg-gray-950"
+                    className="text-white text-lg hover:bg-green-950"
                     component={Link}
                     href="/categories/networking/switches"
                   />
@@ -144,24 +148,24 @@ export default function RootLayout({ children }) {
               </NavLink>
               <NavLink
                 label="Cart"
-                icon={<RiDashboard3Line size={28} color="white" />}
-                className="text-white text-lg hover:bg-gray-950"
+                icon={<FaCartShopping size={28} color="white" />}
+                className="text-white text-lg hover:bg-green-950"
                 variant="subtle"
                 component={Link}
                 href="/cart"
               />
               <NavLink
                 label="Checkout"
-                icon={<RiDashboard3Line size={28} color="white" />}
-                className="text-white text-lg hover:bg-gray-950"
+                icon={<MdOutlineShoppingCartCheckout size={28} color="white" />}
+                className="text-white text-lg hover:bg-green-950"
                 variant="subtle"
                 component={Link}
                 href="/checkout"
               />
             </div>
           </div>
-          {/* <div className="main-section flex flex-col w-5/6">
-            <div className="top-section">
+          <div className="main-section flex flex-col w-5/6">
+            {/* <div className="top-section">
               <div className="flex flex-row justify-between px-10 my-2.5">
                 <MdMenuOpen size={48} />
                 {user ? (
@@ -185,8 +189,9 @@ export default function RootLayout({ children }) {
               </div>
               <hr className="border-zinc-900" />
             </div>
-            {user ? <div className="main-body px-11 py-5">{children}</div> : <LoginPage handleIsLoggedIn={handleIsLoggedIn}/>}
-          </div> */}
+            {user ? <div className="main-body px-11 py-5">{children}</div> : <LoginPage handleIsLoggedIn={handleIsLoggedIn}/>} */}
+            <div className="main-body px-11 py-5">{children}</div>
+          </div>
           </div>
         </div>
       </body>
