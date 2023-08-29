c8d7a1aa32351b82b3765cb4028ef1dfeae2e4ce
koome <frankoomef@gmail.com>
2023-08-29 07:51:14 +0300

    created the homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,12 +4,12 @@ import { Inter } from 'next/font/google'
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FaComputer, FaUserGear, FaChevronRight, FaComputerMouse, FaCartPlus, FaCartShopping } from "react-icons/fa6";
-import { RiDashboard3Line, RiUserSettingsLine, RiComputerFill } from "react-icons/ri";
-import { MdMenuOpen, MdAccountCircle, MdOutlineShoppingCartCheckout, MdStorage, MdComputer, MdOutlineDesktopWindows } from "react-icons/md";
+import { RiUserSettingsLine, RiComputerFill, RiRouterFill } from "react-icons/ri";
+import { MdMenuOpen, MdAccountCircle, MdOutlineShoppingCartCheckout, MdStorage, MdComputer, MdOutlineDesktopWindows, MdMenu } from "react-icons/md";
 import { HiSwitchVertical } from "react-icons/hi"
-import { FcVideoProjector } from "react-icons/fc"
+import { FcHeadset, FcVideoProjector } from "react-icons/fc"
 import { PiComputerTowerFill } from "react-icons/pi"
-import { BsKeyboardFill, BsFillCartCheckFill, BsFillCartDashFill } from "react-icons/bs"
+import { BsKeyboardFill, BsFillCartCheckFill, BsFillCartDashFill, BsFillRouterFill } from "react-icons/bs"
 import { AiFillHome } from "react-icons/ai"
 import { BiCategory, BiSolidServer } from "react-icons/bi"
 import { TbLicense } from "react-icons/tb";
@@ -22,71 +22,29 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   const router = useRouter();
+  const [navCollapsed, setNavCollapsed] = useState(false);
 
 
 
   return (
     <html lang="en">
-      <body className="">
-      <div> 
-        <div className="m-x-6 flex font-bold text-lg text-white sticky overflow-x-hidden w-full min-h-fit justify-between navbar bg-green-800">
-          <h1 className='justify-center ms-10'>
-            <Link href="/">Mburus Tech</Link>
-          </h1>
-          <p className="flex me-10 ">
-            <MdAccountCircle size={28} color="white" title='Logged in user' /> Franklin 
-          </p>
-        </div>
-        <div className="flex flex-row">
-          <div className="navbar bg-green-800 w-1/6 h-screen overflow-y-auto">
-            <div className="links-container flex flex-col justify-start py-2">
-              <NavLink
-                label="Home Page"
-                icon={<AiFillHome size={28} color="white" />}
-                className="text-white text-lg hover:bg-green-950"
-                variant="subtle"
-                component={Link}
-                href="/"
-              />
-              <NavLink
-                label="Account"
-                icon={<RiUserSettingsLine size={28} color="white" />}
-                className="text-white text-lg hover:bg-green-950"
-                variant="subtle"
-                component={Link}
-                href="/user"
-              />
-              <NavLink
-                label="Categories"
-                icon={<BiCategory size={28} color="white" />}
-                className="text-white text-lg hover:bg-green-950"
-                variant="subtle"
-                rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
-                childrenOffset={20}
-              >
+      <body>
+        <div 
+        > 
+          <div className="flex flex-row">
+          {!navCollapsed ?
+            <div className="navbar bg-green-800 w-96 h-screen overflow-y-auto sticky bottom-0 top-0 left-0">               
+              <div className="links-container flex flex-col justify-start py-2">
                 <NavLink
-                  label="Desktops"
-                  icon={<PiComputerTowerFill size={28} color="white" />}
+                  label="Home Page"
+                  icon={<AiFillHome size={28} color="white" />}
                   className="text-white text-lg hover:bg-green-950"
+                  variant="subtle"
                   component={Link}
-                  href="/categories/desktops"
+                  href="/"
                 />
                 <NavLink
-                  label="Monitors"
-                  icon={<MdOutlineDesktopWindows size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/monitors"
-                />
-                <NavLink
-                  label="Laptops"
-                  icon={<MdComputer size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/laptops"
-                />
-                <NavLink
-                  label="Accessories"
+                  label="Categories"
                   icon={<BiCategory size={28} color="white" />}
                   className="text-white text-lg hover:bg-green-950"
                   variant="subtle"
@@ -94,104 +52,174 @@ export default function RootLayout({ children }) {
                   childrenOffset={20}
                 >
                   <NavLink
-                  label="Mouse"
-                  icon={<FaComputerMouse size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/accessories/mouse"
-                />
-                <NavLink
-                  label="Keyboards"
-                  icon={<BsKeyboardFill size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/accessories/keyboards"
-                />
-                <NavLink
-                  label="Storage"
-                  icon={<MdStorage size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/accessories/storage"
-                />
-                </NavLink>
-                <NavLink
-                  label="Projectors"
-                  icon={<FcVideoProjector size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/projectors"
-                />
-                <NavLink
-                  label="Networking"
-                  icon={<BiSolidServer size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  variant="subtle"
-                  rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
-                  childrenOffset={20}
-                >
-                  <NavLink
-                  label="Servers"
-                  icon={<BiSolidServer size={28} color="white" />}
-                  className="text-white text-lg hover:bg-green-950"
-                  component={Link}
-                  href="/categories/networking/servers"
-                  />
-                  <NavLink
-                    label="Switches"
-                    icon={<HiSwitchVertical size={28} color="white" />}
+                    label="Desktops"
+                    icon={<PiComputerTowerFill size={28} color="white" />}
                     className="text-white text-lg hover:bg-green-950"
                     component={Link}
-                    href="/categories/networking/switches"
+                    href="/categories/desktops"
                   />
+                  <NavLink
+                    label="Monitors"
+                    icon={<MdOutlineDesktopWindows size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/monitors"
+                  />
+                  <NavLink
+                    label="Laptops"
+                    icon={<MdComputer size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/laptops"
+                  />
+                  <NavLink
+                    label="Accessories"
+                    icon={<BiCategory size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    variant="subtle"
+                    rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
+                    childrenOffset={20}
+                  >
+                    <NavLink
+                    label="Mouse"
+                    icon={<FaComputerMouse size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/accessories/mouse"
+                  />
+                  <NavLink
+                    label="Keyboards"
+                    icon={<BsKeyboardFill size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/accessories/keyboards"
+                  />
+                  <NavLink
+                    label="Storage"
+                    icon={<MdStorage size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/accessories/storage"
+                  />
+                  <NavLink
+                    label="Headsets"
+                    icon={<FcHeadset size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/accessories/headsets"
+                  />
+                  </NavLink>
+                  <NavLink
+                    label="Projectors"
+                    icon={<FcVideoProjector size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/projectors"
+                  />
+                  <NavLink
+                    label="Networking"
+                    icon={<BiSolidServer size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    variant="subtle"
+                    rightSection={<FaChevronRight size="0.8rem" stroke={1.5} />}
+                    childrenOffset={20}
+                  >
+                    <NavLink
+                    label="Servers"
+                    icon={<BiSolidServer size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/networking/servers"
+                    />
+                    <NavLink
+                      label="Switches"
+                      icon={<HiSwitchVertical size={28} color="white" />}
+                      className="text-white text-lg hover:bg-green-950"
+                      component={Link}
+                      href="/categories/networking/switches"
+                    />
+                    <NavLink
+                    label="Routers"
+                    icon={<BsFillRouterFill size={28} color="white" />}
+                    className="text-white text-lg hover:bg-green-950"
+                    component={Link}
+                    href="/categories/networking/servers"
+                    />
+                    <NavLink
+                      label="Access Points"
+                      icon={<RiRouterFill size={28} color="white" />}
+                      className="text-white text-lg hover:bg-green-950"
+                      component={Link}
+                      href="/categories/networking/switches"
+                    />
+                  </NavLink>
                 </NavLink>
-              </NavLink>
-              <NavLink
-                label="Cart"
-                icon={<FaCartShopping size={28} color="white" />}
-                className="text-white text-lg hover:bg-green-950"
-                variant="subtle"
-                component={Link}
-                href="/cart"
-              />
-              <NavLink
-                label="Checkout"
-                icon={<MdOutlineShoppingCartCheckout size={28} color="white" />}
-                className="text-white text-lg hover:bg-green-950"
-                variant="subtle"
-                component={Link}
-                href="/checkout"
-              />
-            </div>
-          </div>
-          <div className="main-section flex flex-col w-5/6">
-            {/* <div className="top-section">
-              <div className="flex flex-row justify-between px-10 my-2.5">
-                <MdMenuOpen size={48} />
-                {user ? (
-                  <h1 className="font-bold text-xl self-end mx-2">
-                    Welcome, {user.user["firstname"]}
-                  </h1>
-                ) : null}
-                <div className="user-menu flex flex-row">
-                  <FaUserGear size={48} className="mx-2" />
-                  {user ? (
-                    <h1
-                      className="font-bold text-xl self-end mx-2 cursor-pointer"
-                      onClick={handleLogout}
-                    >
-                      Logout
-                    </h1>
-                  ) : (
-                    null
-                  )}
-                </div>
+                <NavLink
+                  label="Cart"
+                  icon={<FaCartShopping size={28} color="white" />}
+                  className="text-white text-lg hover:bg-green-950"
+                  variant="subtle"
+                  component={Link}
+                  href="/cart"
+                />
+                <NavLink
+                  label="Checkout"
+                  icon={<MdOutlineShoppingCartCheckout size={28} color="white" />}
+                  className="text-white text-lg hover:bg-green-950"
+                  variant="subtle"
+                  component={Link}
+                  href="/checkout"
+                />
               </div>
-              <hr className="border-zinc-900" />
             </div>
-            {user ? <div className="main-body px-11 py-5">{children}</div> : <LoginPage handleIsLoggedIn={handleIsLoggedIn}/>} */}
-            <div className="main-body px-11 py-5">{children}</div>
-          </div>
+            : <div className='hidden'></div>
+          }
+            <div className="main-section flex flex-col overflow-x-hidden">
+              <div className="sticky top-0 right-0 left-0 m-x-6 flex font-bold text-lg text-white min-h-fit max-h-fit justify-between bg-green-800">
+              <button
+              onClick={() => setNavCollapsed(!navCollapsed)}>
+                {navCollapsed ?
+                  <MdMenu size={28} color='white'/>
+                  :
+                  <MdMenuOpen size={28} color='white'/>
+                }
+              </button>
+                <h1 className='justify-center ms-10'>
+                  <Link href="/">Mburus Tech</Link>
+                </h1>
+                <Link className="flex me-10 "
+                href={"/user"}
+                >
+                  <RiUserSettingsLine size={28} color="white" title='Logged in user' /> Franklin 
+                </Link>
+              </div>
+              {/* <div className="top-section">
+                <div className="flex flex-row justify-between px-10 my-2.5">
+                  <MdMenuOpen size={48} />
+                  {user ? (
+                    <h1 className="font-bold text-xl self-end mx-2">
+                      Welcome, {user.user["firstname"]}
+                    </h1>
+                  ) : null}
+                  <div className="user-menu flex flex-row">
+                    <FaUserGear size={48} className="mx-2" />
+                    {user ? (
+                      <h1
+                        className="font-bold text-xl self-end mx-2 cursor-pointer"
+                        onClick={handleLogout}
+                      >
+                        Logout
+                      </h1>
+                    ) : (
+                      null
+                    )}
+                  </div>
+                </div>
+                <hr className="border-zinc-900" />
+              </div>
+              {user ? <div className="main-body px-11 py-5">{children}</div> : <LoginPage handleIsLoggedIn={handleIsLoggedIn}/>} */}
+              <div className="main-body">{children}</div>
+              </div>
           </div>
         </div>
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,88 @@
-import { useRouter } from 'next/navigation'
+"use client"
+import DataCard from "@/components/DataCard";
+import { useEffect, useState } from "react";
 
+export default function HomePage() {
+  const [ items, setItems ] = useState([]);
+  const [ laptops, setLaptops ] = useState([]);
+  const [ desktops, setDesktops ] = useState([]);
+  const [ networking, setNetworking ] = useState([]);
+  const [ monitors, setMonitors ] = useState([]);
+  const [ accessories, setAccessories ] = useState([]);
+  
 
-function homePage() {
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await fetch("http://127.0.0.1:3001/items", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          // Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await res.json();
+      setItems(data);
+    };
+    fetchItems();
+  }, []);
 
+  useEffect (() => {  
+    setLaptops(items.filter(item => item.category==="laptops"));
+    setDesktops(items.filter(item => item.category==="desktops"));
+    setMonitors(items.filter(item => item.category==="monitors"));
+    setNetworking(items.filter(item => item.category==="networking"));
+    setAccessories(items.filter(item => item.category==="accessories"));
+  }, [items]);
+  
+  const keys = ["id", "name", "price", "description", "category"]
 	return (
-		<div>
-			Hello
-		</div>
-	)
+    <div>
+      <div>
+        <h1 className="bg-blue-600 text-white text-xl w-full text-center" id="desktops">Desktops</h1>
+        <div className="flex flex-row h-max overflow-x-auto gap-1 my-2">
+          <DataCard
+          items={desktops}
+          keys={keys}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="bg-blue-600 text-white text-xl w-full text-center" id="laptops">Laptops</h1>
+        <div className="flex flex-row h-max overflow-x-auto gap-1 my-2">
+          <DataCard
+          items={laptops}
+          keys={keys}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="bg-blue-600 text-white text-xl w-full text-center">Monitors</h1>
+        <div className="flex flex-row h-max overflow-x-auto gap-1 my-2">
+          <DataCard
+          items={monitors}
+          keys={keys}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="bg-blue-600 text-white text-xl w-full text-center">Networking</h1>
+        <div className="flex flex-row h-max overflow-x-auto gap-1 my-2">
+          <DataCard
+          items={networking}
+          keys={keys}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="bg-blue-600 text-white text-xl w-full text-center">Accessories</h1>
+        <div className="flex flex-row h-max overflow-x-auto gap-1 my-2">
+          <DataCard
+          items={accessories}
+          keys={keys}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
-export default homePage;
