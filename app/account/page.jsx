ac5d44ba6a29a2ab21973385fc784d6eac587dfb
koome <frankoomef@gmail.com>
2023-09-06 01:15:46 +0300

    created user account page

---
+++ app/account/page.jsx
@@ -0,0 +1,67 @@
+"use client"
+import { useEffect, useState } from "react";
+import { BiCart } from "react-icons/bi";
+import { FaTruckMoving } from "react-icons/fa6";
+import { RiOrderPlayFill } from "react-icons/ri";
+
+
+export default function UserProfile() {
+  const token = JSON.parse(sessionStorage.getItem("user_token")).key;
+  const [ userDetails, setUserDetails ] = useState(null);
+  const [ formData, setFormData ] = useState({
+    username: "",
+    first_name: "",
+    sir_name: "",
+    last_name: "",
+    password: "****",
+    phone_number: "",
+  });
+  
+  useEffect (() => {
+    const fetchUserDetails = async() => {
+      const res = await fetch(`http://localhost:8888/api/users`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Token ${token}`,
+        },
+      });
+      const data = await res.json();
+      setUserDetails(data);
+    }
+    fetchUserDetails();
+  }, []);
+  const orders = 3
+
+  return (
+    <>
+    <div className="">
+      <div className="h-1/3">
+
+      </div>
+      <div className="flex flex-row gap-2 font-bold upercase text-xl">
+        <div className="bg-red-200 flex flex-row rounded">
+          <RiOrderPlayFill size={30} />
+          Orders {orders}
+        </div>
+        <div className="bg-blue-200 flex flex-row rounded">
+          <FaTruckMoving size={30}/>
+          In Transit {orders}
+        </div>
+        <div className="bg-green-200 flex flex-row rounded">
+          <BiCart size={30}/>
+          Delivered {orders}
+        </div>
+      </div>
+      <div className="">
+        <form>
+          <div className="col-span-2">
+            
+          </div>
+        </form>
+      </div>
+    </div>
+    </>
+  )
+}
